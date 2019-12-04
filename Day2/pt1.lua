d91b5544f7a9d71de0c13f9bbe5f9f25e027d817
Matthew W <fatboychummy@gmail.com>
2019-12-03 23:18:34 -0700

    commentations

--- Day2/pt1.lua
+++ Day2/pt1.lua
@@ -5,6 +5,11 @@ local function run(day)
     data[i] = 0
   end
 
+  -- opcode functions
+  -- 1 = add
+  -- 2 = mult
+  -- 99 = stop
+  -- any other should cause "attempt to call nil value" error
   local opcodes = {
     [1] = function(a, b, out)
       data[out] = data[a] + data[b]
@@ -20,18 +25,20 @@ local function run(day)
     end
   }
 
+  -- read the file, set up an iterator
   local file = io.open(day .. "input.txt", 'r')
   local iter = string.gmatch(file:read("*a"), "%d+")
   file:close()
 
+  -- initialize to the starting values
   data[1] = 12
   data[2] = 2
 
   repeat
-    local a = tonumber(iter())
-    local b = tonumber(iter())
-    local c = tonumber(iter())
-    local d = tonumber(iter())
+    local a = tonumber(iter()) -- opcode
+    local b = tonumber(iter()) -- position 1
+    local c = tonumber(iter()) -- position 2
+    local d = tonumber(iter()) -- output position
   until opcodes[a](b, c, d)
 
   return data[0]
